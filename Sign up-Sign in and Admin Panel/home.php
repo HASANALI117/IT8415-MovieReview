1729eb53495153f13467a2e24b972c09d69c6a45
hmmm2121 <hussainnader771@gmail.com>
2026-06-02 02:05:05 +0300

    Created Log in - Sign up and Admin Manage user

---
+++ Sign up-Sign in and Admin Panel/home.php
@@ -0,0 +1,20 @@
+<?php
+include 'Header.php';
+
+echo '<br />';
+echo '<h1>Welcome to the home page</h1>';
+
+$Username = $_SESSION['Username'];
+$Role = $_SESSION['Role'];
+
+echo "<br /><h3>Hello $Username</h3>";
+echo "<p>Your role: $Role</p>";
+
+if ($Role == 'admin') {
+    echo '<p><a href="view_users.php" class="navlink">Manage Users</a></p>';
+}
+
+echo '</br><a href="logout.php" class="navlink">Logout</a><br />';
+
+include 'Footer.php';
+?>
---
+++ Sign up-Sign in and Admin Panel/logout.php
@@ -0,0 +1,26 @@
+<?php
+
+$page_title = 'Logout';
+
+include 'Header.php';
+
+echo '<h1>Logged out</h1>';
+
+if (!isset($_SESSION['Username']) )
+{
+     require_once('LoginFunctions.php');
+     $url = absolute_url();
+     header("Location: $url");
+     exit();
+}
+else{
+   $_SESSION = array();
+   session_destroy();
+   setcookie('PHPSESSID','', time()-3600,'/','',0,0);
+}
+
+echo '<br /><h2>you are now logged out </h2>';
+echo '<a href="index.php" class="navlink">Log in</a><br />';
+
+include 'Footer.php';
+?>
